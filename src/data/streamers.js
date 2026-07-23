d20c408fac9cbf18d8921ca79daa38f84cb42508
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2026-07-24 00:07:18 +0200

    [UPD] imgs part 2

--- src/data/streamers.js
+++ src/data/streamers.js
@@ -1,20 +1,127 @@
-import tumblurrImage from "../assets/foto-streamers/tumblurr.webp";
+import attrixImage from "../assets/foto-streamers/attrix.webp";
+import ayarBaffoImage from "../assets/foto-streamers/ayar-baffo.webp";
+import brizz94Image from "../assets/foto-streamers/brizz94.webp";
+import bugalallaImage from "../assets/foto-streamers/bugalalla.webp";
+import chiaraImage from "../assets/foto-streamers/chiara.webp";
+import ciccioGamerImage from "../assets/foto-streamers/ciccio-gamer.webp";
+import cydoniaImage from "../assets/foto-streamers/cydonia.webp";
+import darioMartizImage from "../assets/foto-streamers/dario-martiz.webp";
 import darioMocciaImage from "../assets/foto-streamers/dario-moccia.webp";
-import pow3rImage from "../assets/foto-streamers/pow3r.webp";
+import davidRubinoImage from "../assets/foto-streamers/david-rubino.webp";
+import deluxImage from "../assets/foto-streamers/delux.webp";
+import en3rixImage from "../assets/foto-streamers/en3rix.webp";
+import enkkImage from "../assets/foto-streamers/enkk.webp";
+import favijImage from "../assets/foto-streamers/favij.webp";
+import fierikImage from "../assets/foto-streamers/fierik.webp";
+import frenehImage from "../assets/foto-streamers/freneh.webp";
+import gabboDsqImage from "../assets/foto-streamers/gabbo-dsq.webp";
 import grenbaudImage from "../assets/foto-streamers/grenbaud.webp";
-import yotobiImage from "../assets/foto-streamers/yotobi.webp";
+import gskiantoImage from "../assets/foto-streamers/gskianto.webp";
 import homyatolImage from "../assets/foto-streamers/homyatol.webp";
+import ilGabbroneImage from "../assets/foto-streamers/il-gabbrone.webp";
+import ilGattoSulTuboImage from "../assets/foto-streamers/il-gatto-sul-tubo.webp";
+import ilMasseoImage from "../assets/foto-streamers/il-masseo.webp";
+import ilRossoImage from "../assets/foto-streamers/il-rosso.webp";
+import imVioletImage from "../assets/foto-streamers/im-violet.webp";
+import japahefImage from "../assets/foto-streamers/japahef.webp";
+import jokerImage from "../assets/foto-streamers/joker.webp";
+import kertusImage from "../assets/foto-streamers/kertus.webp";
+import kroatomistImage from "../assets/foto-streamers/kroatomist.webp";
+import kurolilyImage from "../assets/foto-streamers/kurolily.webp";
+import lolloLacustreImage from "../assets/foto-streamers/lollo-lacustre.webp";
+import lyonWgfImage from "../assets/foto-streamers/lyon-wgf.webp";
+import manuuxoImage from "../assets/foto-streamers/manuuxo.webp";
+import marcoMerrinoImage from "../assets/foto-streamers/marco-merrino.webp";
 import matteoHsImage from "../assets/foto-streamers/matteo-hs.webp";
-import zanoImage from "../assets/foto-streamers/zano.webp";
+import moonrydeImage from "../assets/foto-streamers/moonryde.webp";
+import nanniImage from "../assets/foto-streamers/nanni.webp";
+import nefastoneImage from "../assets/foto-streamers/nefastone.webp";
+import nezakImage from "../assets/foto-streamers/nezak.webp";
 import paoloCannoneImage from "../assets/foto-streamers/paolo-cannone.webp";
+import peshImage from "../assets/foto-streamers/pesh.webp";
+import pizImage from "../assets/foto-streamers/piz.webp";
+import pow3rImage from "../assets/foto-streamers/pow3r.webp";
+import queenGiorgiaImage from "../assets/foto-streamers/queen-giorgia.webp";
+import rekinssImage from "../assets/foto-streamers/rekinss.webp";
+import s7ormyImage from "../assets/foto-streamers/s7ormy.webp";
+import sabakuNoMaikuImage from "../assets/foto-streamers/sabaku-no-maiku.webp";
+import sharishaImage from "../assets/foto-streamers/sharisha.webp";
+import teknoydImage from "../assets/foto-streamers/teknoyd.webp";
+import terenasImage from "../assets/foto-streamers/terenas.webp";
+import theRealMarzaImage from "../assets/foto-streamers/the-real-marza.webp";
+import tuberanzaImage from "../assets/foto-streamers/tuberanza.webp";
+import tumblurrImage from "../assets/foto-streamers/tumblurr.webp";
+import veloxImage from "../assets/foto-streamers/velox.webp";
+import vivaLaFazzaImage from "../assets/foto-streamers/viva-la-fazza.webp";
+import xiuderImage from "../assets/foto-streamers/xiuder.webp";
+import yoshi93Image from "../assets/foto-streamers/yoshi93.webp";
+import yotobiImage from "../assets/foto-streamers/yotobi.webp";
+import zanoImage from "../assets/foto-streamers/zano.webp";
+import zazzoneImage from "../assets/foto-streamers/zazzone.webp";
 
 const streamers = [
   {
-    id: "tumblurr",
-    name: "Tumblurr",
-    twitchUsername: "tumblurr",
-    image: tumblurrImage,
-    categories: ["Just Chatting", "Gaming"],
+    id: "attrix",
+    name: "Attrix",
+    twitchUsername: "attrix",
+    image: attrixImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "ayar-baffo",
+    name: "AyarBaffo",
+    twitchUsername: "ayarbaffo",
+    image: ayarBaffoImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "brizz94",
+    name: "Brizz94",
+    twitchUsername: "brizz94",
+    image: brizz94Image,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "bugalalla",
+    name: "Bugalalla",
+    twitchUsername: "bugalalla",
+    image: bugalallaImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "chiara",
+    name: "Chiara",
+    twitchUsername: "cydonia_chiara",
+    image: chiaraImage,
+    categories: ["Pokémon", "Gaming"],
+    active: true,
+  },
+  {
+    id: "ciccio-gamer",
+    name: "CiccioGamer89",
+    twitchUsername: "cicciogamer89",
+    image: ciccioGamerImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "cydonia",
+    name: "Cydonia",
+    twitchUsername: "cydonia_chiara",
+    image: cydoniaImage,
+    categories: ["Pokémon", "Gaming"],
+    active: true,
+  },
+  {
+    id: "dario-martiz",
+    name: "Dario Martiz",
+    twitchUsername: "dariomartiz",
+    image: darioMartizImage,
+    categories: ["Gaming", "Just Chatting"],
     active: true,
   },
   {
@@ -26,11 +133,67 @@ const streamers = [
     active: true,
   },
   {
-    id: "pow3r",
-    name: "POW3R",
-    twitchUsername: "pow3rtv",
-    image: pow3rImage,
-    categories: ["FPS", "Gaming"],
+    id: "david-rubino",
+    name: "David Rubino",
+    twitchUsername: "davidrubino",
+    image: davidRubinoImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "delux",
+    name: "Delux",
+    twitchUsername: "delux",
+    image: deluxImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "en3rix",
+    name: "En3rix",
+    twitchUsername: "en3rix",
+    image: en3rixImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "enkk",
+    name: "Enkk",
+    twitchUsername: "enkk",
+    image: enkkImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "favij",
+    name: "Favij",
+    twitchUsername: "favij",
+    image: favijImage,
+    categories: ["Gaming", "Intrattenimento"],
+    active: true,
+  },
+  {
+    id: "fierik",
+    name: "Fierik",
+    twitchUsername: "fierik",
+    image: fierikImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "freneh",
+    name: "Freneh",
+    twitchUsername: "freneh",
+    image: frenehImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "gabbo-dsq",
+    name: "GabboDSQ",
+    twitchUsername: "gabbodsq",
+    image: gabboDsqImage,
+    categories: ["Gaming", "Just Chatting"],
     active: true,
   },
   {
@@ -42,11 +205,11 @@ const streamers = [
     active: true,
   },
   {
-    id: "yotobi",
-    name: "Yotobi",
-    twitchUsername: "yotobi",
-    image: yotobiImage,
-    categories: ["Just Chatting", "Cinema"],
+    id: "gskianto",
+    name: "Gskianto",
+    twitchUsername: "gskianto",
+    image: gskiantoImage,
+    categories: ["Gaming", "Just Chatting"],
     active: true,
   },
   {
@@ -58,6 +221,118 @@ const streamers = [
     active: true,
   },
   {
+    id: "il-gabbrone",
+    name: "Il Gabbrone",
+    twitchUsername: "ilgabbrone",
+    image: ilGabbroneImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "il-gatto-sul-tubo",
+    name: "Il Gatto Sul Tubo",
+    twitchUsername: "ilgattosultubo",
+    image: ilGattoSulTuboImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "il-masseo",
+    name: "Il Masseo",
+    twitchUsername: "ilmasseo",
+    image: ilMasseoImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "il-rosso",
+    name: "Il Rosso",
+    twitchUsername: "ilrossopiubelloditwitch",
+    image: ilRossoImage,
+    categories: ["Just Chatting", "Gaming"],
+    active: true,
+  },
+  {
+    id: "im-violet",
+    name: "ImViolet",
+    twitchUsername: "imviolet",
+    image: imVioletImage,
+    categories: ["Just Chatting", "Gaming"],
+    active: true,
+  },
+  {
+    id: "japahef",
+    name: "Japahef",
+    twitchUsername: "just_japahef",
+    image: japahefImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "joker",
+    name: "J0k3R",
+    twitchUsername: "j0k3r",
+    image: jokerImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "kertus",
+    name: "Kertus",
+    twitchUsername: "kertus",
+    image: kertusImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "kroatomist",
+    name: "Kroatomist",
+    twitchUsername: "kroatomist",
+    image: kroatomistImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "kurolily",
+    name: "Kurolily",
+    twitchUsername: "kurolily",
+    image: kurolilyImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "lollo-lacustre",
+    name: "Lollo Lacustre",
+    twitchUsername: "lollolacustre",
+    image: lolloLacustreImage,
+    categories: ["Just Chatting", "Gaming"],
+    active: true,
+  },
+  {
+    id: "lyon-wgf",
+    name: "Lyon WGF",
+    twitchUsername: "lyonwgflive",
+    image: lyonWgfImage,
+    categories: ["Gaming", "Intrattenimento"],
+    active: true,
+  },
+  {
+    id: "manuuxo",
+    name: "ManuuXO",
+    twitchUsername: "manuuxo",
+    image: manuuxoImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "marco-merrino",
+    name: "Marco Merrino",
+    twitchUsername: "marcomerrino",
+    image: marcoMerrinoImage,
+    categories: ["Just Chatting", "Gaming"],
+    active: true,
+  },
+  {
     id: "matteo-hs",
     name: "Matteo HS",
     twitchUsername: "matteohs",
@@ -66,11 +341,35 @@ const streamers = [
     active: true,
   },
   {
-    id: "zano",
-    name: "Zano XVII",
-    twitchUsername: "zanoxvii",
-    image: zanoImage,
-    categories: ["Fifa", "Gaming"],
+    id: "moonryde",
+    name: "Moonryde",
+    twitchUsername: "moonryde",
+    image: moonrydeImage,
+    categories: ["FPS", "Gaming"],
+    active: true,
+  },
+  {
+    id: "nanni",
+    name: "Nanni",
+    twitchUsername: "nannitwitch",
+    image: nanniImage,
+    categories: ["Just Chatting", "Gaming"],
+    active: true,
+  },
+  {
+    id: "nefastone",
+    name: "Nefastone",
+    twitchUsername: "nefastone",
+    image: nefastoneImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "nezak",
+    name: "NeZaK",
+    twitchUsername: "nezak_",
+    image: nezakImage,
+    categories: ["Gaming", "Just Chatting"],
     active: true,
   },
   {
@@ -78,7 +377,167 @@ const streamers = [
     name: "Paolo Cannone",
     twitchUsername: "paoloidolo",
     image: paoloCannoneImage,
-    categories: ["LOL", "Just Chatting"],
+    categories: ["League of Legends", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "pesh",
+    name: "Pesh",
+    twitchUsername: "pesh",
+    image: peshImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "piz",
+    name: "Piz",
+    twitchUsername: "piz",
+    image: pizImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "pow3r",
+    name: "POW3R",
+    twitchUsername: "pow3r",
+    image: pow3rImage,
+    categories: ["FPS", "Gaming"],
+    active: true,
+  },
+  {
+    id: "queen-giorgia",
+    name: "Queen Giorgia",
+    twitchUsername: "queen_giorgia",
+    image: queenGiorgiaImage,
+    categories: ["Just Chatting", "Gaming"],
+    active: true,
+  },
+  {
+    id: "rekinss",
+    name: "Rekinss",
+    twitchUsername: "rekinss",
+    image: rekinssImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "s7ormy",
+    name: "S7ORMy",
+    twitchUsername: "s7ormytv",
+    image: s7ormyImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "sabaku-no-maiku",
+    name: "Sabaku no Maiku",
+    twitchUsername: "sabakunomaiku",
+    image: sabakuNoMaikuImage,
+    categories: ["Videogiochi", "Approfondimento"],
+    active: true,
+  },
+  {
+    id: "sharisha",
+    name: "Sharisha",
+    twitchUsername: "sharishaxd",
+    image: sharishaImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "teknoyd",
+    name: "Teknoyd",
+    twitchUsername: "teknoyd",
+    image: teknoydImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "terenas",
+    name: "Terenas",
+    twitchUsername: "terenas",
+    image: terenasImage,
+    categories: ["Gaming", "Esports"],
+    active: true,
+  },
+  {
+    id: "the-real-marza",
+    name: "TheRealMarza",
+    twitchUsername: "therealmarza",
+    image: theRealMarzaImage,
+    categories: ["Just Chatting", "Gaming"],
+    active: true,
+  },
+  {
+    id: "tuberanza",
+    name: "Tuberanza",
+    twitchUsername: "tuberanza",
+    image: tuberanzaImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "tumblurr",
+    name: "Tumblurr",
+    twitchUsername: "tumblurr",
+    image: tumblurrImage,
+    categories: ["Just Chatting", "Gaming"],
+    active: true,
+  },
+  {
+    id: "velox",
+    name: "Velox",
+    twitchUsername: "velox",
+    image: veloxImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "viva-la-fazza",
+    name: "VivaLaFazza",
+    twitchUsername: "vivalafazza",
+    image: vivaLaFazzaImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "xiuder",
+    name: "Xiuder",
+    twitchUsername: "xiuder",
+    image: xiuderImage,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "yoshi93",
+    name: "Yoshi93",
+    twitchUsername: "yoshi93",
+    image: yoshi93Image,
+    categories: ["Gaming", "Just Chatting"],
+    active: true,
+  },
+  {
+    id: "yotobi",
+    name: "Yotobi",
+    twitchUsername: "yotobi",
+    image: yotobiImage,
+    categories: ["Just Chatting", "Cinema"],
+    active: true,
+  },
+  {
+    id: "zano",
+    name: "Zano XVII",
+    twitchUsername: "zanoxvii",
+    image: zanoImage,
+    categories: ["EA Sports FC", "Gaming"],
+    active: true,
+  },
+  {
+    id: "zazzone",
+    name: "Zazzone230",
+    twitchUsername: "zazzone230",
+    image: zazzoneImage,
+    categories: ["Gaming", "Just Chatting"],
     active: true,
   },
 ];
