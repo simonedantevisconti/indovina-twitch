4a6257de20a57a76d441bf543b019364fe219224
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2026-07-23 02:05:14 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import DefaultLayout from "./layouts/DefaultLayout"
+import Homepage from "./pages/Homepage"
+import Game from "./pages/Game"
+
+function App() {
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const DefaultLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default DefaultLayout;
